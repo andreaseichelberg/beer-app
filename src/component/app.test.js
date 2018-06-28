5fd4670bed0013a940b023817e51bb513c8a3f01
Andreas Eichelberg <Andi@Andreass-iMac.home>
2018-06-28 20:40:41 +0200

    remove header

--- src/component/app.test.js
+++ src/component/app.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Search from './Search';
+import DetailProduct from './DetailProduct';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Search />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<DetailProduct />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
